4215337778b89e91004b69596d3daa4d9ebb2ad9
Barthelemy <barthelemy.von.haller@cern.ch>
2016-12-15 13:46:23 +0100

    Update the config file examples to match the new scheme.

--- config/config.example.php
+++ config/config.example.php
@@ -1,32 +1,38 @@
 <?php
 /** Jira Webhook Listener
-*
-* A small set of scripts to catch Webhook notifications from JIRA and run custom commands on a per-project basis
-*
-* Copyright (c) 2014 B Tasker
-* Modified by B von Haller (2016)
-* Released under GNU GPL V2, see LICENSE
-*
-* Configuration File example
-* Copy this file and remove the "example" from its name to use it. 
-*/
+ *
+ * A small set of scripts to catch Webhook notifications from JIRA and run custom commands on a per-project basis
+ *
+ * Copyright (c) 2014 B Tasker
+ * Modified by B von Haller (2016)
+ * Released under GNU GPL V2, see LICENSE
+ *
+ * Configuration File example
+ * Copy this file and remove the "example" from its name to use it. 
+ */
 
-
-
-
-$configuration_array = array(
-
-  'EXAMPLE' => array(
-
-	      'fireon' => 'newissue,updatedissue',
-	      'actions' => array(
-		  'email' => 'foo@example.com,bar@example.com',
-		  'urlrequest' => 'refreshIndex'
-	      ),
-	      'ProjectURLBase' => 'http://example.com/projects', // No trailing slash
-	      'ProjectURLSuffix' => '.html'
-	  )
-
-
-
+// The variable must be called configuration_array
+// It is made of an array <project_name> => <parameters>.
+// The parameters themselves are an array specifying the type of event and actions.
+// One can use wildcards "*" instead but then no mapping of specific projects can be done.
+$configuration_array = array (
+		
+		'EXAMPLE' => array (
+				
+				'fireon' => 'newissue,updatedissue',
+				'actions' => array (
+						'email' => 'foo@example.com,bar@example.com',
+						'urlrequest' => 'refreshIndex' 
+				),
+				'ProjectURLBase' => 'http://example.com/projects', // No trailing slash
+				'ProjectURLSuffix' => '.html' 
+		)/* , 
+		'*' => array ( // this only works if there is no other project defined
+				'fireon' => '*',
+				'actions' => array (
+						'email' => '' // no option here
+				),
+				'ProjectURLBase' => 'http://dev8.its.cern.ch/jira/projects', // No trailing slash
+				'ProjectURLSuffix' => '.html'
+		) */
 );
--- config/email.action.config.example.php
+++ config/email.action.config.example.php
@@ -11,7 +11,55 @@
 * Copy this file and remove the "example" from its name to use it. 
 */
 
-
-
-
-$conf->smtpserver = 'example.com';
+// The variable defined here must be called "email_configurations". 
+// It is made of an array of <template_name> => <email_parameters>.
+// Each template must specify the fields: from, to, body and subject. 
+// In case one wants to send an HTML email the extra field "content_type" 
+// must be set to "text/html".
+$email_configurations = array(
+		'datesupport' => array(
+				'from' => 'alice.jira@cern.ch',
+				'to' => 'bvonhall@cern.ch', //alice-datesupport@cern.ch
+		  		'body' => "   Project:   " . $this->request->getIssueProject() .
+				"\r\nReporter:  " . $this->request->getIssueReporter() .
+				"\r\nAssignee:  " . $this->request->getIssueAssignee() .
+				"\r\nCreated:   " . $this->request->getIssueCreated() .
+				"\r\nDetails:   https://alice.its.cern.ch/jira/browse/" . $this->request->getIssueKey() .
+				"\r\n" .
+				"\r\n###########" .
+				"\r\n# TITLE" .
+				"\r\n###########" .
+				"\r\n" . $this->request->getIssueTitle() .
+				"\r\n" .
+				"\r\n###########" .
+				"\r\n# BODY" .
+				"\r\n###########" .
+				"\r\n" . $this->request->getIssueDescription() .
+				"\r\n",
+				'subject' => "(" . $this->request->getIssueKey() . ")" . $this->request->getIssueTitle()//,
+				//   		'content_type' => ""
+		),
+		'daq_intervention' => array(
+				'from' => 'alice.jira@cern.ch',
+				'to' => 'bvonhall@cern.ch', //alice-datesupport@cern.ch
+				'body' => "   Project:    " . $this->request->getIssueProject() .
+				"\r\nReporter:   " . $this->request->getIssueReporter() .
+				"\r\nAssignee:   " . $this->request->getIssueAssignee() .
+				"\r\nCreated:    " . $this->request->getIssueCreated() .
+				"\r\nSubsystems: " . $this->request->issue->field->subsystems .
+				"\r\nDetails:    https://alice.its.cern.ch/jira/browse/" . $this->request->getIssueKey() .
+				"\r\nResolved:   " . $this->request->getIssueCreated() .
+				"\r\n" .
+				"\r\n###########" .
+				"\r\n# TITLE" .
+				"\r\n###########" .
+				"\r\n" . $this->request->getIssueTitle() .
+				"\r\n" .
+				"\r\n###########" .
+				"\r\n# BODY" .
+				"\r\n###########" .
+				"\r\n" . $this->request->getIssueDescription() .
+				"\r\n",
+				'subject' => "New Intervention: " . $this->request->getIssueTitle()
+		)
+);
